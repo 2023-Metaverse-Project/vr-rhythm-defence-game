fb402919dd0ffff1fb67f2337cc0cd36b8602089
sgj0402 <122610159+sgj0402@users.noreply.github.com>
2023-12-20 18:13:25 +0900

    몬스터 개발 중

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,11 +10,32 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private Transform target;
 
+    [SerializeField]
+    private float maxHP = 100;
+    private float currentHP;
+
     private void Awake()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
 
         agent.SetDestination(target.position);
         agent.speed *= Random.Range(0.8f, 1.5f);
+
+        currentHP = maxHP;
+    }
+
+    public void DecreaseHP(float damage)
+    {
+        currentHP -= damage;
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
